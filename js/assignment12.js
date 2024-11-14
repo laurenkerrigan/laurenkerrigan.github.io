0e3549bc4c0a15c2ac35117a3428f627d834448b
laurenkerrigan <lkerrigan75@gmail.com>
2024-11-14 10:37:49 -0700

    starting assignment 12

---
+++ js/assignment12.js
@@ -0,0 +1,29 @@
+let myData = {};
+
+function fetchData() {
+    fetch("https://corsproxy.io?https://xkcd.com/614/info.0.json");
+
+    .then(res => {
+        if (res.ok) {
+        return res.json();
+    }
+
+    else {
+        console.log(res);
+    }
+    })
+
+    .then(res => {
+        myData = res;
+        console.log(myData);
+
+        //title
+        document.getElementById
+        //displayingthe comic
+        document.getElementById("comic").src = myData.img;
+        //document.getElementById("comic").
+    })
+
+}
+
+fetchData();
